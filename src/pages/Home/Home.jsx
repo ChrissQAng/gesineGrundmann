6f125f181b0766fae5bdefc0fbf2f99fcd00224d
ChrissAngerer <goodomen@outlook.de>
2024-11-11 12:26:17 +0100

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,24 @@ const Home = () => {
     <>
       <h1>Gesine Grundmann</h1>
       <br />
-      <img src="/./public/images/image.png" alt="" />
-      <a className="about" href="/about">
-        about
-      </a>
-      <a href="/contact">contact</a>
-      <a href="/texts">texts</a>
-      <a href="/views">views</a>
-      <a href="/works">works</a>
+      <div className="container">
+        <img src="/./public/images/image.png" alt="" />
+        <a className="about" href="/about">
+          about
+        </a>
+        <a className="contact" href="/contact">
+          contact
+        </a>
+        <a className="texts" href="/texts">
+          texts
+        </a>
+        <a className="views" href="/views">
+          views
+        </a>
+        <a className="works" href="/works">
+          works
+        </a>
+      </div>
     </>
   );
 };
