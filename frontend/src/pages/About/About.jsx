5a6294a6b5fe7c24e8ab2f997cc45b7f818a649d
ChrissAngerer <goodomen@outlook.de>
2024-12-15 12:56:39 +0100

    about done

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -4,6 +4,114 @@ const About = () => {
   return (
     <div className="about-wrapper">
       <h2>about</h2>
+      <h3>Gesine Grundmann, geb. 1974, lebt und arbeitet in Köln</h3>
+      <p>
+        Studium an der Kunstakademie Düsseldorf (bei R. Trockel, T. Cragg, H.
+        Kiecol)
+      </p>
+      <p>
+        <span>2001-02</span>MA Fine Art, Goldsmiths College, London
+      </p>
+      <p>
+        <span>2005</span>Meisterschülerin bei Prof. Rosemarie Trockel
+      </p>
+      <h4>Lehrerfahrung</h4>
+      <p>
+        <span>2007-10</span>Lehrauftrag für Freie Kunst an der HBK Braunschweig
+      </p>
+      <p>
+        <span></span>im Rahmen des Dorothea-Erxleben-Stipendiums
+      </p>
+      <p>
+        <span>2009 –13</span>Lehrassistenz bei Prof. Trockel, Kunstakadamie
+        Düsseldorf
+      </p>
+      <p>
+        <span></span>(Krankheitsvertretung im Wintersemester 2010/2011)
+      </p>
+      <p>
+        <span>2012-14</span>Lehrauftrag für Bildhauerei an der Kunstakademie
+        Düsseldorf
+      </p>
+      <p>
+        <span>2014-15</span>Lehrauftrag Praxis zeitgenössischer Kunst,
+        Ruhrakademie Schwerte
+      </p>
+      <p>
+        <span>2017-18</span>artist in residence, Leitung des Kunstlabors,
+      </p>
+      <p>
+        <span></span>A.-Coppel-Gesamtschule Solingen
+      </p>
+      <p>
+        <span>2024 SS</span>Keramik, Künstlerisches Gestalten, Institut für
+        Künstlerische Keramik und Glas IKKG, Höhr-Grenzhausen
+      </p>
+      <h4>Stipendien, Preise, Residencies</h4>
+      <p>
+        <span>2024</span>copy (machine) in residence, Museum für Photokopie,
+        Mülheim a.d. Ruhr
+      </p>
+
+      <p>
+        <span>2023</span>Kunstpreis der Freunde des Kunstwerks
+      </p>
+      <p>
+        <span></span>Recherchestipendium der Stadt Köln
+      </p>
+      <p>
+        <span>2022</span>Atelierstipendium Salzamt Linz, Österreich
+      </p>
+      <p>
+        <span>2021</span>Lichtenberg Studios Berlin
+      </p>
+      <p>
+        <span>2020</span>Neustart-Stipendium, Stiftung Kunstfonds
+      </p>
+      <p>
+        <span>2015</span>Katalogförderung Kunststiftung NRW
+      </p>
+      <p>
+        <span>2013</span>Ida Gerhardi Förderpreis, Lüdenscheid
+      </p>
+      <p>
+        <span>2013</span>nominiert für Nominated for 5x5 Castellò, Spanien
+      </p>
+      <p>
+        <span>2012</span>Arbeitsstipendium der Stiftung Kunstfonds
+      </p>
+      <p>
+        <span>2011</span>Projektstipendium der Kunststiftung NRW
+      </p>
+      <p>
+        <span>2009-10</span>Erwine Steinblum Stipendium, Sylt
+      </p>
+      <p>
+        <span>2008</span>Peter-Mertes-Stipendium, Bonner Kunstverei
+      </p>
+      <p>
+        <span>2008-09</span>Tel Aviv Stipendium der Kunststiftung NRW, Bronner
+        Stiftung
+      </p>
+      <p>
+        <span></span>Artist Studios Tel Aviv und Goethe Institut Tel Aviv,
+        Israel
+      </p>
+      <p>
+        <span>2007-09</span>Dorothea-Erxleben-Stipendium, HBK Braunschweig
+      </p>
+      <p>
+        <span>2007</span>Künstlerstipendium der Kunststiftung NRW
+      </p>
+      <p>
+        <span>2004–08</span>Atelierstipendium des Kölnischen Kunstvereins
+      </p>
+      <p>
+        <span>2002</span>Auslandsstipendium der Kunststiftung NRW
+      </p>
+      <p>
+        <span>2001</span>DAAD Stipendium für London
+      </p>
     </div>
   );
 };
